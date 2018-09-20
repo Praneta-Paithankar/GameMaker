913972972d1d6ece7fb1163b5d9ba05ff696fcb0
Praneta Paithankar <ppaithan@iu.edu>
2018-09-20 00:12:03 -0400

    Add model

---
+++ src/main/java/com/components/GameElement.java
@@ -0,0 +1,78 @@
+package com.components;
+
+import java.awt.Color;
+import java.awt.image.BufferedImage;
+
+import com.dimension.Dimension;
+import com.infrastruture.Action;
+import com.infrastruture.Drawable;
+
+public class GameElement {
+	private int width;
+	private int height;
+	private Dimension position;
+	Color color;
+	private BufferedImage image;
+	private Action action;
+	private Drawable draw;
+	
+	public GameElement() {
+		
+	}
+
+	public int getWidth() {
+		return width;
+	}
+
+	public void setWidth(int width) {
+		this.width = width;
+	}
+
+	public int getHeight() {
+		return height;
+	}
+
+	public void setHeight(int height) {
+		this.height = height;
+	}
+
+	public Dimension getPosition() {
+		return position;
+	}
+
+	public void setPosition(Dimension position) {
+		this.position = position;
+	}
+
+	public Color getColor() {
+		return color;
+	}
+
+	public void setColor(Color color) {
+		this.color = color;
+	}
+
+	public BufferedImage getImage() {
+		return image;
+	}
+
+	public void setImage(BufferedImage image) {
+		this.image = image;
+	}
+
+	public Action getAction() {
+		return action;
+	}
+
+	public void setAction(Action action) {
+		this.action = action;
+	}
+
+	public Drawable getDraw() {
+		return draw;
+	}
+
+	public void setDraw(Drawable draw) {
+		this.draw = draw;
+	}
+}
---
+++ src/main/java/com/infrastruture/Drawable.java
@@ -0,0 +1,8 @@
+package com.infrastruture;
+
+import com.components.GameElement;
+
+public interface Drawable {
+
+	void draw(GameElement element);
+}
