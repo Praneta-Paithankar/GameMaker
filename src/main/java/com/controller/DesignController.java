7530f9ff23ce31668105804c4cfe37ac0cb14de8
chetan253 <cpatil0@outlook.com>
2018-09-22 16:20:44 -0400

    Added elements to test the game

--- src/main/java/com/controller/DesignController.java
+++ src/main/java/com/controller/DesignController.java
@@ -36,9 +36,9 @@ public class DesignController {
 		mainJframe = gui;
 		graphicsElements = new ArrayList<>();
 		timerElements  = new ArrayList<>();
+		colliders = new ArrayList<>();
 		keyboardElements = new HashMap<>();
 		controlElements = new HashMap<>();
-		colliders = new ArrayList<>();
 	}
 	
 	public List<GameElement> getKeyboardElementsBasedKeys(int key)
@@ -53,24 +53,30 @@ public class DesignController {
 	public void addGameElement() {
 
 		// gui.getData();
-		GameElement elementPaddle = new GameElement(new Dimensions(80,10), new Coordinate(100, 350), "Paddle", MoveType.LEFTRIGHT);
+		GameElement elementPaddle = new GameElement(new Dimensions(80,10), new Coordinate(300, 350), "Paddle", MoveType.LEFTRIGHT);
 		elementPaddle.setColor(Color.BLACK);
 		elementPaddle.setDraw(new DrawRectangularColorShape());
+		elementPaddle.setVelX(20);
+		elementPaddle.setVisible(true);
 		
 		GameElement elementBall =  new GameElement(new Dimensions(15), new Coordinate(50, 50), "Ball", MoveType.FREE);
 		elementBall.setColor(Color.RED);
 		elementBall.setDraw(new DrawOvalColor());
+		elementBall.setVelX(1);
+		elementBall.setVelY(1);
+		elementBall.setVisible(true);
 		
 		GameElement elementBrick1 = new GameElement(new Dimensions(50, 25), new Coordinate(250, 90), "Brick", MoveType.FIXED);
 		elementBrick1.setColor(Color.BLUE);
 		elementBrick1.setDraw(new DrawRectangularColorShape());
+		elementBrick1.setVisible(true);
 		
-		GameElement elementBrick2 = new GameElement(new Dimensions(50, 25), new Coordinate(370, 90), "Brick", MoveType.FIXED);
+		GameElement elementBrick2 = new GameElement(new Dimensions(50, 25), new Coordinate(563, 79), "Brick", MoveType.FIXED);
 		elementBrick2.setColor(Color.BLUE);
 		elementBrick2.setDraw(new DrawRectangularColorShape());
+		elementBrick2.setVisible(true);
 		
 		clock = new Clock();
-		
 		
 		// add element into elements
 		graphicsElements.add(elementPaddle);
@@ -80,17 +86,17 @@ public class DesignController {
 		
 		timerElements.add(elementBall);
 		
-//		keyboardElements.put(KeyEvent.VK_LEFT, new ArrayList<GameElement>(Arrays.asList(elementPaddle)));
-//		keyboardElements.put(KeyEvent.VK_RIGHT, new ArrayList<GameElement>(Arrays.asList(elementPaddle)));
-//		
-//		controlElements.put("START", ActionType.START);
-//		controlElements.put("PAUSE", ActionType.PAUSE);
-//		controlElements.put("UNDO", ActionType.UNDO);
-//		controlElements.put("SAVE", ActionType.SAVE);
-//		controlElements.put("LOAD", ActionType.LOAD);
-//		controlElements.put("REPLAY", ActionType.REPLAY);
-//		controlElements.put("CHANGELAYOUT", ActionType.CHANGELAYOUT);
-//		
+		keyboardElements.put(KeyEvent.VK_LEFT, new ArrayList<GameElement>(Arrays.asList(elementPaddle)));
+		keyboardElements.put(KeyEvent.VK_RIGHT, new ArrayList<GameElement>(Arrays.asList(elementPaddle)));
+		
+		controlElements.put("START", ActionType.START);
+		controlElements.put("PAUSE", ActionType.PAUSE);
+		controlElements.put("UNDO", ActionType.UNDO);
+		controlElements.put("SAVE", ActionType.SAVE);
+		controlElements.put("LOAD", ActionType.LOAD);
+		controlElements.put("REPLAY", ActionType.REPLAY);
+		controlElements.put("CHANGELAYOUT", ActionType.CHANGELAYOUT);
+		
 		CollisionChecker collisionChecker = new CollisionChecker();
 		Collider ballPaddle = new Collider(elementBall, elementPaddle, CollisionType.BOUNCE, CollisionType.FIXED, collisionChecker);
 		Collider ballBrick1 = new Collider(elementBall, elementBrick1, CollisionType.BOUNCE, CollisionType.EXPLODE, collisionChecker);
@@ -99,8 +105,7 @@ public class DesignController {
 		colliders.add(ballPaddle);
 		colliders.add(ballBrick1);
 		colliders.add(ballBrick2);
-//		
-//		
+
 		// add elements into gamePanel
 		mainJframe.getGamePanel().addComponent(elementPaddle);
 		mainJframe.getGamePanel().addComponent(elementBall);
@@ -113,18 +118,12 @@ public class DesignController {
 		// update timer Elements or KeyboardElements
 	}
 	
-	
-	public void addControlElement() {
-		
-		CustomButton button = mainJframe.getDesignPanel().getButton();
-	    controlElements.put(button.getActionCommand(), button.getActionType());
-	    mainJframe.getControlPanel().addComponent(button);
-	    mainJframe.getControlPanel().add(button);
-	    //		controlElements.put(button.getActionCommand(),button.getActionType());
-		// gui.getData();
-		// add element into elements
-		// add elements into controlPanel with actions
-	    mainJframe.getControlPanel().revalidate();
+	public List<GameElement> getElements() {
+		return graphicsElements;
+	}
+
+	public void setElements(List<GameElement> elements) {
+		this.graphicsElements = elements;
 	}
 
 	public List<GameElement> getTimerElements() {
@@ -133,6 +132,13 @@ public class DesignController {
 
 	public void setTimerElements(List<GameElement> timerElements) {
 		this.timerElements = timerElements;
+	}
+
+	public void addControlElement() {
+		
+		// gui.getData();
+		// add element into elements
+		// add elements into controlPanel with actions
 	}
 
 	public Clock getClock() {
@@ -158,20 +164,6 @@ public class DesignController {
 	public void setColliders(List<Collider> colliders) {
 		this.colliders = colliders;
 	}
-	public List<GameElement> getGraphicsElements() {
-		return graphicsElements;
-	}
-
-	public void setGraphicsElements(List<GameElement> graphicsElements) {
-		this.graphicsElements = graphicsElements;
-	}
-
-	public HashMap<Integer, List<GameElement>> getKeyboardElements() {
-		return keyboardElements;
-	}
-
-	public void setKeyboardElements(HashMap<Integer, List<GameElement>> keyboardElements) {
-		this.keyboardElements = keyboardElements;
-	}
+	
 	
 }
