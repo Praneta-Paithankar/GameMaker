9763b78ab18ea09daffbd12998fc471b60330af7
liyun <liyun@10.0.0.137>
2018-09-22 12:06:34 -0400

    type mistake fix

--- src/main/java/com/helper/ActionType.java
+++ src/main/java/com/helper/ActionType.java
@@ -1,7 +1,7 @@
 package com.helper;
 
 public enum ActionType {
-	SVAE,LOAD,PLAY,PAUSE,REPLAY;
+	SAVE,LOAD,PLAY,PAUSE,REPLAY;
 	private static String [] representStr = {"SAVE","LOAD","PLAY","PAUSE", "REPLAY"};
 	
 	public static String [] getActionTypes() {
